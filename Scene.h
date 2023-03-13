8941e600202efe2dc6f5defad08c84ef14d496a3
AndreB20 <andreiburlacu2000@gmail.com>
2023-03-13 20:38:12 +0200

    Next load

---
+++ Scene.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Shader.h"
+#include "Camera.h"
+#include "Model.h"
+
+class Scene
+{
+	Scene()
+	{
+
+	}
+
+	void initPos()
+	{
+
+	}
+
+};
